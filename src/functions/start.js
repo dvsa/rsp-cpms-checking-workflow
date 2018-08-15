986766c9bbc501cd56aca3a35d5ef2b9111bad6a
Ammar Haider <ammar.haider@bjss.com>
2018-08-15 17:04:34 +0100

    starts execution and passes data along to check

--- src/functions/start.js
+++ src/functions/start.js
@@ -1,5 +1,19 @@
+import { StepFunctions } from 'aws-sdk';
+
+const stepfunctions = new StepFunctions();
+
 export default (event, context, callback) => {
-	console.log('Logging incoming event');
-	console.log(event);
-	callback(null, 'success');
+	const stateMachineArn = process.env.statemachine_arn;
+	const params = {
+		stateMachineArn,
+	};
+
+	return stepfunctions.startExecution(params).promise().then(() => {
+		callback(null, {
+			message: `Your statemachine ${stateMachineArn} executed successfully`,
+			data: event,
+		});
+	}).catch((error) => {
+		callback(error.message);
+	});
 };
