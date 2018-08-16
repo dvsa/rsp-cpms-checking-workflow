2eaca67d17f8c215c3a6bf852e24d8020bc80567
Ammar Haider <ammar.haider@bjss.com>
2018-08-16 11:53:24 +0100

    makes signed requests to payments to check if record exists

--- src/config.js
+++ src/config.js
@@ -3,9 +3,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sqsUrl = process.env.SQS_URL;
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const paymentServiceUrl = process.env.PAYMENT_SERVICE_URL;
+const cpmsServiceUrl = process.env.CPMS_SERVICE_URL;
 
 const config = {
 	sqsUrl,
+	clientId,
+	clientSecret,
+	paymentServiceUrl,
+	cpmsServiceUrl,
 };
 
 export default config;
--- src/functions/check.js
+++ src/functions/check.js
@@ -1,5 +1,19 @@
+import SignedHttpClient from '../utils/httpClient';
+import appConfig from '../config';
+import isEmptyObject from '../utils/isEmptyObject';
+
+const cpmsHttpClient = new SignedHttpClient(appConfig.cpmsServiceUrl);
+const paymentHttpClient = new SignedHttpClient(appConfig.paymentServiceUrl);
+
 export default (event, context, callback) => {
-	console.log('Logging incoming event');
-	console.log(event);
-	callback(null, 'success');
+	const { MessageId } = event.messageData;
+	const { ReceiptReference, IsGroupPayment, PaymentCode } = event.paymentData;
+	// Check if payment was recorded in the appropriate payments table;
+	const getPaymentPath = IsGroupPayment ? 'groupPayments' : 'payments';
+	paymentHttpClient.get(`${getPaymentPath}/${PaymentCode}`)
+		.then((item) => {
+			if (isEmptyObject(item)) return callback(null, { paymentRecordFound: false });
+			return callback(null, { paymentRecordFound: true });
+		})
+		.catch(err => callback(err));
 };
