86b6a35ae7bda92ec4d046ffb02eeb97fcfa46f7
Ammar Haider <ammar.haider@bjss.com>
2018-08-23 16:04:19 +0100

    fixes looking up payment in getPaymentRecord method

--- src/functions/checkForOrphanedPayments.js
+++ src/functions/checkForOrphanedPayments.js
@@ -21,14 +21,14 @@ export default async (event, context, callback) => {
 		console.log('messageAttributes');
 		console.log(parseMessageAttributes(messageAttributes));
 		// Check if the payment is in the payments table
-		const item = await paymentsService.getPaymentRecord(IsGroupPayment, PenaltyId, PenaltyType);
+		const payment = await paymentsService.getPaymentRecord(IsGroupPayment, PenaltyId, PenaltyType);
 		// Exit and delete message off the queue
-		return callback(null, item);
+		return callback(null, payment);
 	} catch (getPaymentRecordError) {
-		// If the item doesn't exist, check in cpms
+		// If the payment item doesn't exist, check in cpms
 		const notFoundErrors = [`Payment for type: ${PenaltyType} not found in item`, 'Item not found'];
 		const { message } = getPaymentRecordError;
-		if (notFoundErrors.includes(message) || getPaymentRecordError.response.status === 404) {
+		if (notFoundErrors.includes(message)) {
 			try {
 				const { code, auth_code } = await cpmsService.confirm(PenaltyType, ReceiptReference); // eslint-disable-line
 				console.log('code from lambda');
